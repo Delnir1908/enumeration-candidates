67e6c666f33b06604b9fce27fdb888eb983a2218
Jason Chen <windson7@gmail.com>
2025-07-19 20:42:35 -0700

    code refactored to correct errors and delete unneccessary statements

--- filters.rb
+++ filters.rb
@@ -1,45 +1,30 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
 
-  def find(id)
+  def find(id, candidates)
     # Your code Here
-    candidates.select.with_index {|candidate| candidate[:id] == id}.map(&:first)
+    candidates.find {|candidate| candidate[:id] == id}
   end
   
   def experienced?(candidate)
     # Your code Here
-    if candidate[:years_of_experience] >= 2
-      true
-    else
-      false
+    candidate[:years_of_experience] >= 2
   end
 
   def github_over_100pts?(candidate)
-    if candidate[:github_points] >= 100
-      true
-    else
-      false
+    candidate[:github_points] >= 100
   end
 
   def know_ruby_or_python?(candidate)
-    if candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python')
-      true
-    else
-      false
+    candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python')
   end
 
   def apply_within_15_days?(candidate)
-    if Date.today - candidate[:date_applied] <= 15
-      true
-    else
-      false
+    Date.today - candidate[:date_applied] <= 15
   end
 
   def adult?(candidate)
-    if candidate[:age] >= 18
-      true
-    else
-      false
+    candidate[:age] >= 18
   end
 
   def qualified_candidates(candidates)
@@ -61,6 +46,6 @@
 
   def ordered_by_qualifications(candidates)
     candidates.sort { |a, b| 
-    [a[:years_of_experience], a[:github_points]] 
-      <=> [b[:years_of_experience], b[:github_points]] }
+    [-a[:years_of_experience], -a[:github_points]] 
+      <=> [-b[:years_of_experience], -b[:github_points]] }
   end
