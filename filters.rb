51c29695b35764f9009272d986d3ed4a9772c14b
Jason Chen <windson7@gmail.com>
2025-07-19 20:27:52 -0700

    add find(id) method

--- filters.rb
+++ filters.rb
@@ -1,16 +1,8 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
 
-def find(id)
+  def find(id)
     # Your code Here
+    candidates.select.with_index {|candidate| candidate[:id] == id}.map(&:first)
   end
-  
-  def experienced?(candidate)
-    # Your code Here
-  end
-  
-  def qualified_candidates(candidates)
-    # Your code Here
-  end
-  
-  # More methods will go below
+
