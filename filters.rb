6308fadee6eac0caa13d73069f2cb3947f55c2c1
Jason Chen <windson7@gmail.com>
2025-07-19 20:29:45 -0700

    add helper method to find qualified candidates

--- filters.rb
+++ filters.rb
@@ -13,3 +13,50 @@
     else
       false
   end
+
+  def github_over_100pts?(candidate)
+    if candidate[:github_points] >= 100
+      true
+    else
+      false
+  end
+
+  def know_ruby_or_python?(candidate)
+    if candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python')
+      true
+    else
+      false
+  end
+
+  def apply_within_15_days?(candidate)
+    if Date.today - candidate[:date_applied] <= 15
+      true
+    else
+      false
+  end
+
+  def adult?(candidate)
+    if candidate[:age] >= 18
+      true
+    else
+      false
+  end
+
+  def qualified_candidates(candidates)
+    # Your code Here
+    qualified_candidates_list = []
+    candidates.each do |candidate| 
+      if experienced?(candidate) 
+          && github_over_100pts?(candidate) 
+          && know_ruby_or_python?(candidate)
+          && apply_within_15_days?(candidate)
+          && adult?(candidate)       
+        qualified_candidates_list.push(candidate)
+      end
+    end
+    qualified_candidates_list
+  end
+  
+  # More methods will go below
+
+
